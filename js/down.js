bb982e1602a132433692633971983a385ecebbf4
Kahilo Massango <kahilo42@gmail.com>
2024-05-25 17:39:35 +0100

    initials

---
+++ js/down.js
@@ -0,0 +1,10 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1200,
+    });
+    
